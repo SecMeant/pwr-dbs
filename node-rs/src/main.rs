d0fe07c315f56d47a9cad19e39fe36257fb450e4
SecMeant <secmeant@gmail.com>
2020-03-26 01:37:10 +0100

    [node-rs] Node registration works

--- node-rs/src/main.rs
+++ node-rs/src/main.rs
@@ -5,11 +5,20 @@ use protos::delegate::*;
 extern crate websocket;
 
 use std::env;
-use std::fmt;
 use websocket::client::ClientBuilder;
+use websocket::dataframe::DataFrame;
+
+type Websock = websocket::sync::Client<std::net::TcpStream>;
 
 fn usage(program_name: &String) {
     println!("Usage: {} <host> <port> <resource>", program_name);
+}
+
+fn recv_protobuf<T : Message>(ws: &mut Websock) -> T {
+    let indata = ws.recv_dataframe().unwrap().data;
+    println!("Got data: {:?}", indata);
+
+    parse_from_bytes::<T>(&indata).unwrap()
 }
 
 fn main() {
@@ -25,11 +34,32 @@ fn main() {
     let resource = &args[3];
     let url = format!("ws://{}:{}{}", host, port, resource);
 
-    let client = ClientBuilder::new(&url)
+    let mut ws = ClientBuilder::new(&url)
         .unwrap()
         .add_protocol("rust-websocket")
         .connect_insecure()
         .unwrap();
 
     println!("Connected to {}", url);
+
+    let mut register_request = RegisterNodeRequest::default();
+    register_request.set_version(1);
+    let outdata = websocket::Message::binary(register_request.write_to_bytes().unwrap());
+    ws.send_message(&outdata).unwrap();
+
+    // For some reason first read is not blocking (?)
+    // To restore blocking behavior simply read once and expect to fail.
+    // TODO(holz) obv check why it returns empty slice.
+    let d = ws.recv_dataframe().unwrap().data;
+    if !d.is_empty() {
+        panic!("Expected first read to fail.");
+    }
+
+    let register_response = recv_protobuf::<RegisterNodeResponse>(&mut ws);
+
+    match register_response.get_code() {
+        0 => { println!("Node registered"); },
+        _ => { panic!("Node registration failed"); }
+    }
+
 }
